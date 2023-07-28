5541c857a402dae22f1c183fe89ac22c53c4ceb8
Jubril Babatunde <babatundejubril@gmail.com>
2023-07-28 09:22:50 +0100

    Webpack

---
+++ 0x00-Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,5 @@
+import './footer.css';
+
+const $ = require('jquery');
+
+$('body').append('<p>Copyright - Holberton School</p>');
---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,8 @@
+import './header.css';
+
+const $ = require('jquery');
+
+$('body').append("<div id='logo'></div>");
+$('body').append('<h1>Holberton Dashboard</h1>');
+
+console.log('Init header');
